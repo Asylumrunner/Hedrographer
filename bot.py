9e1b52f09b2406987b43774d4265f0703a2baaba
Asylumrunner <asylumrunner@gmail.com>
2020-03-14 11:22:18 -0700

    Add number of character toggle, name lists

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import discord
 import requests
 import json
+from names import drow_names
 from random import randrange
 from secrets import secret_dict, maskmaker_url
 
@@ -37,20 +38,39 @@ async def on_message(message):
             chat_message += "{}\n```".format(result)
 
             await client.send_message(message.channel, chat_message)
-        except Exception:
-            await client.send_message(message.channel, "Floor dice don't count")
+        except Exception as e:
+            await client.send_message(message.channel, "Floor dice don't count. (There was an exception: {})".format(e))
     if message.content.startswith('!g'):
-        #try:
-        split_message = message.content.split(" ")
-        response = requests.post(maskmaker_url, json={"number": 1, "attributes": ["Pride", "Intellect", "Weirdness", "Strength", "Paranoia"]}).text
-        response = json.loads(response)
-        chat_message = "```"
-        for character in response['characters']:
-            chat_message += "Name: {}\n".format(character['name'])
-            chat_message += "Traits: {}\n".format(character['traits'])
-            chat_message += "Attributes: {}\n".format(character['attribute'])
-        chat_message += "```"
-        await client.send_message(message.channel, chat_message)
-        #except Exception:
-        #    await client.send_message(message.channel, "Uhh... his name is.... uhhhh.... Dave. The Elf.")
+        try:
+            split_message = message.content.split(" ")
+            try:
+                number = int(split_message[1])
+            except ValueError:
+                number = 1
+            
+            try:
+                species = split_message[2] if split_message[2] in ['Drow', 'Aelfir', 'Human'] else 'Drow'
+            except Exception:
+                species = 'Drow'
+            
+            if species == 'Drow':
+                name_list = drow_names
+            elif species == 'Aelfir':
+                name_list = drow_names
+            else:
+                name_list = drow_names
+            response = requests.post(maskmaker_url, json={"number": number, "names": name_list, "attributes": ["Pride", "Intellect", "Weirdness", "Strength", "Paranoia"]}).text
+            response = json.loads(response)
+            chat_message = "```"
+            if "--just-names" in split_message:
+                chat_message += "{}\n".format([character['name'] for character in response['characters']])
+            else:
+                for character in response['characters']:
+                    chat_message += "Name: {}\n".format(character['name'])
+                    chat_message += "Traits: {}\n".format(character['traits'])
+                    chat_message += "Attributes: {}\n".format(character['attribute'])
+            chat_message += "```"
+            await client.send_message(message.channel, chat_message)
+        except Exception as e:
+            await client.send_message(message.channel, "Uhh... his name is.... uhhhh.... Dave. The Elf. (There was an exception: {})".format(e))
 client.run(secret_dict['client_key'])
